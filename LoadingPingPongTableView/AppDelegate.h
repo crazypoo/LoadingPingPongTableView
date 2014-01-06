4888d50564ad04a3e73087fb46bfcd38d4571f48
crazypoo <273277355@qq.com>
2014-01-06 10:26:36 +0800

    Initial Commit

---
+++ LoadingPingPongTableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LoadingPingPongTableView
+//
+//  Created by crazypoo on 1/6/14.
+//  Copyright (c) 2014 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LoadingPingPongTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoadingPingPongTableView
+//
+//  Created by crazypoo on 1/6/14.
+//  Copyright (c) 2014 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
